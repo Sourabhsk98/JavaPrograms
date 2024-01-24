5a174f17c74633632f9846bab76c628cc21ba5d3
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-01-24 20:21:29 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/RandomNumber.java
@@ -0,0 +1,15 @@
+package com.JavaPrograms;
+
+public class RandomNumber {
+    public static void main(String[] args)
+    {
+// Generating random numbers.
+// Java program to demonstrate working of
+// Math.random() to generate random numbers.
+// Math.random() always provide output between 0 and 1.
+        System.out.println("1st RandomNumber Number: " + Math.random());
+        System.out.println("2nd RandomNumber Number: " + Math.random());
+        System.out.println("3rd RandomNumber Number: " + Math.random());
+        System.out.println("4th RandomNumber Number: " + Math.random());
+    }
+}
