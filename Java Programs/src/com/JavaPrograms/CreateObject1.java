ab784d8c5fd8b83cef724db0a2766e98880bc52c
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-01-26 21:28:15 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/CreateObject1.java
@@ -0,0 +1,15 @@
+package com.JavaPrograms;
+
+// Creating an object using new keyword by invoking the constructor (default or parametrized) of the class.
+public class CreateObject1 {
+    //constructor of the class
+    CreateObject1()
+    {
+        System.out.println("Jai Shree Ram");
+    }
+    public static void main(String[] args)
+    {
+//creating an object using new keyword
+        CreateObject1 o = new CreateObject1();
+    }
+}
