14e8219129a80d6bde3e93eff6c68c31ad8a4483
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-01-27 19:00:19 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/PrintAsciiValue3.java
@@ -0,0 +1,11 @@
+package com.JavaPrograms;
+
+public class PrintAsciiValue3 {
+    public static void main(String []args)
+    {
+        for(char ch='A';ch<='Z';ch++)
+        {
+            System.out.println("The ASCII value of "+ch+" is "+(int)ch);
+        }
+    }
+}
