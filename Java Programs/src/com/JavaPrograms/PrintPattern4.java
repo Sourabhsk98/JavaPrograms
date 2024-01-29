eef0f4787c79c058afd01b2eb7d33d937bd23c8d
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-01-29 21:28:29 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/PrintPattern4.java
@@ -0,0 +1,22 @@
+package com.TodaysJavaPrograms;
+
+public class PrintPattern4 {
+    public static void main(String[] args)
+    {
+        int n=5;
+//inner loop
+        for (int i= 0; i<= n; i++)
+        {
+//outer loop
+            for (int j=1; j<=n-i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=0;k<=i;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
