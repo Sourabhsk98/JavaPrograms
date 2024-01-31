c276d9a176977727291a6dbcffb497145d269b29
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-01-31 20:02:40 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/NumberPattern7.java
@@ -0,0 +1,20 @@
+package com.TodaysJavaPrograms;
+
+public class NumberPattern7 {
+    public static void main(String[] args)
+    {
+        int rows=5;
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= rows-i; j++)
+            {
+                System.out.print(1);
+            }
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
