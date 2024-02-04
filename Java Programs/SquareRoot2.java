f34206da39bd91282f25af88f1bddc193f7ecfaa
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-02-04 18:35:41 +0530

    Add files via upload

---
+++ Java Programs/SquareRoot2.java
@@ -0,0 +1,24 @@
+package com.TodaysJavaPrograms;
+
+ /* Java program to demonstrate working
+               of java.lang.Math.sqrt() method */
+public class SquareRoot2 {
+    public static void main(String args[])
+    {
+        int a = 100;
+
+        System.out.println(Math.sqrt(a));
+
+        a = 25;
+
+        System.out.println(Math.sqrt(a));
+
+        a = 64;
+
+        System.out.println(Math.sqrt(a));
+
+        a = 90;
+
+        System.out.println(Math.sqrt(a));
+    }
+}
