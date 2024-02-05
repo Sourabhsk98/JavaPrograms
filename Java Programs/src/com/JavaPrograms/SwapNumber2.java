e10ba5de3c0dd58fd4cf0f497448d4b650a4f6dd
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-02-05 23:01:29 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/SwapNumber2.java
@@ -0,0 +1,18 @@
+package com.TodaysJavaPrograms;
+
+// Swapping two numbers using multiplication and division
+public class SwapNumber2 {
+    public static void main(String args[])
+    {
+        int x = 10;
+        int y = 20;
+        System.out.println("values before swapping:");
+        System.out.println("x = " + x +" y = " + y);
+
+        x = x*y; //now x is 200
+        y = x/y; //now x is 200 and y is 20, on dividing x/y is y=10 (original value of x)
+        x = x/y; //now x is 200 and y is 10, on dividing x/y is x=20 (original value of y)
+        System.out.println("values after swapping:");
+        System.out.println("x = " + x +" y = " + y);
+    }
+}
