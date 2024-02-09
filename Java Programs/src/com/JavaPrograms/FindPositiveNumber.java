e40507ff7c32b5063e19581ae1fd0eeaef0f9d26
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-02-09 13:35:55 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/FindPositiveNumber.java
@@ -0,0 +1,31 @@
+package com.TodaysJavaPrograms;
+
+// Programs to check the number is positive, negative, or zero Using Relational Operator.
+import java.util.Scanner;
+
+public class FindPositiveNumber {
+    public static void main(String[] args)
+    {
+        int num;
+//object of the Scanner class
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+//reading a number from the user
+        num = sc.nextInt();
+//checks the number is greater than 0 or not
+        if(num>0)
+        {
+            System.out.println("The number is positive.");
+        }
+//checks the number is less than 0 or not
+        else if(num<0)
+        {
+            System.out.println("The number is negative.");
+        }
+//executes when the above two conditions return false
+        else
+        {
+            System.out.println("The number is zero.");
+        }
+    }
+}
