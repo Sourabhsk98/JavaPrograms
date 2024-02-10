9ef5960d36258f7aac6bd1376ad8a1213af846a3
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-02-10 10:08:18 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/PrefectSquare2.java
@@ -0,0 +1,24 @@
+package com.TodaysJavaPrograms;
+// Check the number is perfect square or not
+public class PrefectSquare2 {
+    static boolean isPerfectSquare(int x)
+    {
+        if (x >= 0) {
+
+            int sr = (int)Math.sqrt(x);
+
+            return ((sr * sr) == x);
+        }
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+        int x = 84;
+
+        if (isPerfectSquare(x))
+            System.out.print("True");
+        else
+            System.out.print("False");
+    }
+}
