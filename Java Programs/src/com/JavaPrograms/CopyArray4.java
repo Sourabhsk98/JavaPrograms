8e7b00b03ef781b4d9d1b85e1ba52a281aff5245
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-02-13 19:37:41 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/CopyArray4.java
@@ -0,0 +1,28 @@
+package com.TodaysJavaPrograms;
+
+// Java program to demonstrate array copy using Arrays.copyOfRange().
+import java.util.Arrays;
+public class CopyArray4 {
+    public static void main(String[] args)
+    {
+        // Custom input array
+        int a[] = { 1, 8, 3, 5, 9, 10 };
+
+        // Creating an array b[] and
+        // copying elements of a[] to b[]
+        int b[] = Arrays.copyOfRange(a, 2, 6);
+
+        // Changing b[] to verify that
+        // b[] is different from a[]
+
+        // Iterating over array a[]
+        System.out.println("Contents of a[] ");
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+
+        // Iterating over array b[]
+        System.out.println("\n\nContents of b[] ");
+        for (int i = 0; i < b.length; i++)
+            System.out.print(b[i] + " ");
+    }
+}
