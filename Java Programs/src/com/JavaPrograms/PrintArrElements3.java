ce26450a9b46402e170f0b656dd6d5fe93053c9b
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-02-16 20:19:22 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/PrintArrElements3.java
@@ -0,0 +1,11 @@
+package com.TodaysJavaPrograms;
+
+public class PrintArrElements3 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int element: array) {
+            System.out.print(element+" ");
+        }
+    }
+}
