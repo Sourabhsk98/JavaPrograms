9738271e5d6a34e6d1984419df835f97a71a5aa4
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-02-22 22:25:26 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/RightRotate2.java
@@ -0,0 +1,34 @@
+package com.TodaysJavaPrograms;
+
+public class RightRotate2 {
+    public static void main(String[] args) {
+        //Initializing the array numbers
+        int [] numbers = new int [] {1,2,3,4,5,6};
+
+        // This number specifies how many times the array needs to be right rotated.
+
+        int n = 2;
+        System.out.println("Given array is: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        //Right rotate the array by n times
+        for(int i = 0; i < n; i++){
+            int j, lastElement;
+            //Storing the last element of the array to move to start
+            lastElement = numbers[numbers.length-1];
+            for(j = numbers.length-1; j > 0; j--){
+                //Shifting the element to right by 1 on each iteration of loop
+                numbers[j] = numbers[j-1];
+            }
+            //Adding the last element at the beginning of the array
+            numbers[0] = lastElement;
+        }
+        System.out.println();
+        //Printing output array
+        System.out.println("Array after "+n+" right rotations: ");
+        for(int i = 0; i< numbers.length; i++){
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
