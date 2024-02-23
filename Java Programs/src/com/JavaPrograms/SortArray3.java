be9f42c32f5bea6ed2ae7a3bda816a96af80be48
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-02-23 22:52:23 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/SortArray3.java
@@ -0,0 +1,41 @@
+package com.TodaysJavaPrograms;
+
+import java.util.*;
+
+// Sort the given array.
+//Reverse the sorted array.
+public class SortArray3 {
+    public static void main(String[] args)
+    {
+        // Initializing the array
+        int array[] = { 1, 2, 3, 4, 5, 6 };
+
+        // Sorting the array in ascending order
+        Arrays.sort(array);
+
+        // Reversing the array
+        reverse(array);
+
+        // Printing the elements
+        System.out.println(Arrays.toString(array));
+    }
+    public static void reverse(int[] array)
+    {
+        // Length of the array
+        int n = array.length;
+
+        // Swapping the first half elements with last half
+        // elements
+        for (int i = 0; i < n / 2; i++) {
+
+            // Storing the first half elements temporarily
+            int temp = array[i];
+
+            // Assigning the first half to the last half
+            array[i] = array[n - i - 1];
+
+            // Assigning the last half to the first half
+            array[n - i - 1] = temp;
+        }
+    }
+}
