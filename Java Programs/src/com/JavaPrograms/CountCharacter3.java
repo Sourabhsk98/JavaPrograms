2d98bf09e0664242dc49a279769058dd7d066556
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-02-28 22:57:21 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/CountCharacter3.java
@@ -0,0 +1,13 @@
+package com.TodaysJavaPrograms;
+
+import java.util.*;
+public class CountCharacter3 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String str="Hello World.!";
+        System.out.println(
+                "Total number of character in string is "
+                        +str.length());
+    }
+}
