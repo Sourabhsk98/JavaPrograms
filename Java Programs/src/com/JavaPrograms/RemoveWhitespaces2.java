4459006dbdd79b750ef7399bcd8762cb9994b2dc
Sourabh Katti <130495401+Sourabhsk98@users.noreply.github.com>
2024-03-08 17:16:55 +0530

    Add files via upload

---
+++ Java Programs/src/com/JavaPrograms/RemoveWhitespaces2.java
@@ -0,0 +1,11 @@
+package com.TodaysJavaPrograms;
+
+public class RemoveWhitespaces2 {
+    public static void main(String[] args)
+    {
+        String str = "    Hello   World . . . .   !";
+        String op = str.replace(" ","");
+
+        System.out.println(op);
+    }
+}
